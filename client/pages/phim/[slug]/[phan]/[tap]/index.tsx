298e8fdf8d50c979df1f5016f8156656a3e6a6d8
KhanhVy719 <lunalunanek71@gmail.com>
2026-07-01 23:22:06 +0700

    fix(player): block external embed top redirects

--- client/pages/phim/[slug]/[phan]/[tap]/index.tsx
+++ client/pages/phim/[slug]/[phan]/[tap]/index.tsx
@@ -187,6 +187,8 @@ const EMBED_PROVIDERS = [
 
 const SELF_HOSTED_PLAYER_VERSION = "captcha-query-20260630-allow-origin";
 const SELF_HOSTED_IFRAME_SANDBOX = "allow-scripts allow-same-origin allow-forms allow-presentation allow-pointer-lock";
+const EXTERNAL_EMBED_IFRAME_SANDBOX =
+  "allow-scripts allow-same-origin allow-forms allow-presentation allow-pointer-lock allow-storage-access-by-user-activation";
 
 const getUrlOrigin = (url: string) => {
   try {
@@ -937,7 +939,7 @@ const XemPhim: NextPage<IPageProps> = (props) => {
               id="player"
               allow="fullscreen; autoplay; encrypted-media; picture-in-picture; web-share"
               referrerPolicy="no-referrer"
-              sandbox={hasSelfHostedVideo ? SELF_HOSTED_IFRAME_SANDBOX : undefined}
+              sandbox={hasSelfHostedVideo ? SELF_HOSTED_IFRAME_SANDBOX : EXTERNAL_EMBED_IFRAME_SANDBOX}
               src={playerSrc}
               onLoad={() => setIframeLoaded(true)}
             />
